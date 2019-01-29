91a95e635d6bfcd1fb800eef951b675792ba1ec3
shenjinglei <shenjinglei2010@163.com>
2019-01-29 16:16:26 +0800

    feature(test/3.2) 开发Web应⽤：模版引擎与静态资源

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://blog.didispace.com");
+        return "index";
     }
 
 }
