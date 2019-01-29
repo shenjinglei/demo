c07e3a4497fd127e82b7732cc99215bf27f1ed21
shenjinglei <shenjinglei2010@163.com>
2019-01-30 01:27:02 +0800

    feature(test/3.3) Web应⽤的统⼀异常处理

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.exception.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController {
+public class HelloController  {
 
-    @ResponseBody
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    public String hello() throws Exception{
+        throw new Exception("发生错误");
+
+    }
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
     }
 
     @RequestMapping("/")
