66c0d79aa78afedcd848a51849e3d593e0fd7809
shenjinglei <shenjinglei2010@163.com>
2019-02-01 16:51:26 +0800

    feature(test/4.3/4-2-2) 使⽤Log4j记录⽇志

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,38 +1,34 @@
 package com.example;
 
-import com.example.web.HelloController;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.awt.*;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MockServletContext.class)
-@WebAppConfiguration
+@SpringBootTest
 public class ApplicationTests {
-    //测试git
-    private MockMvc mvc;
 
-    @Before
-    public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-    }
+    private Logger logger = Logger.getLogger(getClass());
 
     @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World")));
+    public void test() throws Exception {
+        logger.info("输出info");
+        logger.debug("输出debug");
+        logger.error("输出error");
     }
 
 }
