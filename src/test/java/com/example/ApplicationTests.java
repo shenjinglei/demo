fda56001711d293c41a43aa8a3a6853ef4cd0424
shenjinglei <shenjinglei2010@163.com>
2019-02-01 13:38:07 +0800

    feature(master) 整理master分支，初始化master

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package com.example;
 
+import com.example.web.HelloController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MockServletContext.class)
 @WebAppConfiguration
-public class DemoApplicationTests {
+public class ApplicationTests {
     //测试git
     private MockMvc mvc;
 
