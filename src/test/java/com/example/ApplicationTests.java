dee6637f7eadd07f146e52dc6a4e9e7b5a7d12cb
shenjinglei <shenjinglei2010@163.com>
2019-02-02 12:05:39 +0800

    feature(test/5.1/3-2-1) Spring Boot中使用JdbcTemplate访问数据库

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import com.example.service.UserService;
 import com.example.web.HelloController;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
@@ -17,23 +20,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MockServletContext.class)
-@WebAppConfiguration
+@SpringBootTest()
 public class ApplicationTests {
-    //测试git
-    private MockMvc mvc;
+
+    @Autowired
+    private UserService userService;
 
     @Before
-    public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    public void setUp(){
+        userService.deleteAllUsers();
     }
 
     @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World")));
+    public void test() throws Exception{
+        userService.create("a", 1);
+        userService.create("b", 2);
+        userService.create("c", 3);
+        userService.create("d", 4);
+        userService.create("e", 5);
+
+        Assert.assertEquals(5, userService.getAllUsers().intValue());
+
+        userService.deleteByName("a");
+        userService.deleteByName("e");
+
+        Assert.assertEquals(3, userService.getAllUsers().intValue());
     }
+
+
+//
+//    //测试git
+//    private MockMvc mvc;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+//    }
+//
+//    @Test
+//    public void getHello() throws Exception {
+//        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(equalTo("Hello World")));
+//    }
 
 }
 
