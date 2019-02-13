ff86e29f37a17869887cfd96ee61ca312dd92183
shenjinglei <shenjinglei2010@163.com>
2019-02-13 15:21:48 +0800

    feature(test/StateMachine/6-1-1) SpringBoot 状态机使用练习

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,14 +1,26 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Autowired
+    private StateMachine<States, Events> stateMachine;
+
+    @Override
+    public void run(String... args) throws Exception{
+        stateMachine.start();
+        stateMachine.sendEvent(Events.PAY);
+        stateMachine.sendEvent(Events.RECEIVE);
+    }
 }
 
